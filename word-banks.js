3be0988b5d143ffac02f7841b00bd8818024b015
gregschram <greg@gregschram.com>
2024-11-09 00:01:40 -0800

    Create word-banks.js

---
+++ word-banks.js
@@ -0,0 +1,44 @@
+export const wordBanks = {
+    "one": {
+        words: [
+            "ant", "ask", "bag", "bear", "bee", "bike", "bird", "bloom", "blue", "boat", 
+            "bone", "bow", "brain", "branch", "brush", "bug", "burn", "bus", "cage", "cane", 
+            "care", "cave", "chain", "chalk", "cheek", "chess", "chin", "clay", "coat", "coin", 
+            "comb", "cord", "cow", "crab", "crane", "cry", "deer", "desk", "dice", "dime", 
+            "dive", "door", "dream", "dress", "drum", "duck", "ear", "egg", "eye", "face", 
+            "fan", "farm", "fence", "fin", "flag", "flame", "flute", "fog", "foot", "frame", 
+            "friend", "frog", "game", "gate", "goat", "gold", "grape", "grave", "guard", "hand", 
+            "hat", "hawk", "heart", "heel", "hill", "hook", "horn", "horse", "house", "ink", 
+            "jaw", "key", "knee", "knife", "lake", "leg", "light", "lime", "lip", "lock", 
+            "map", "match", "mice", "milk", "mint", "mist", "moth", "mouse", "nail", "neck"
+        ]
+    },
+    "two": {
+        words: [
+            "apple", "battle", "beetle", "berry", "blanket", "bubble", "bucket", "burger", "butter", "button", 
+            "cactus", "candy", "carrot", "castle", "cherry", "chicken", "circle", "coffee", "coral", "corner", 
+            "cotton", "crayon", "credit", "crystal", "cycle", "dinner", "dolphin", "donkey", "donut", "dragon", 
+            "eagle", "envy", "farther", "feather", "fiction", "figure", "finger", "finish", "firefly", "football", 
+            "freedom", "garden", "garlic", "gentle", "giant", "ginger", "glitter", "goddess", "helpful", "hide", 
+            "honey", "husband", "inject", "insect", "jelly", "jewel", "jumping", "kitten", "level", "lily", 
+            "lion", "little", "lotus", "magic", "maple", "marble", "melon", "mirror", "needle", "ocean", 
+            "olive", "orange", "oyster", "palace", "panel", "parrot", "peach", "peanut", "pebble", "pepper", 
+            "pickle", "pigeon", "pizza", "planet", "pocket", "portal", "potato", "puppy", "rabbit", "rainbow", 
+            "razor", "rescue", "ribbon", "running", "saddle", "sailor", "season", "silver", "simple", "spider"
+        ]
+    },
+    "three": {
+        words: [
+            "accident", "admiral", "adorable", "adventure", "afternoon", "airplane", "algebra", "alphabet", "ambition", "anchor", 
+            "android", "animator", "aquarium", "architect", "asteroid", "attitude", "audience", "audition", "automatic", "avalanche", 
+            "beginning", "bicycle", "birthday", "blossom", "blueberry", "boundary", "broccoli", "buffalo", "butterfly", "cardigan", 
+            "carnival", "carriage", "caterpillar", "champion", "character", "chickadee", "chocolate", "cinnamon", "cityscape", "colorful", 
+            "comedy", "comfortable", "company", "constant", "corridor", "criminal", "crocodile", "cylinder", "delivery", "democrat", 
+            "detective", "diagonal", "digital", "discovery", "dragonfly", "education", "emperor", "engineer", "envelope", "eskimo", 
+            "evaluate", "evidence", "excellent", "factory", "favorite", "february", "fisherman", "general", "government", "graduate", 
+            "hamburger", "harmony", "helmet", "history", "honeybee", "hospital", "identify", "iguana", "incident", "indigo", 
+            "infinite", "innocent", "institute", "interval", "kangaroo", "ladybug", "lavender", "lettuce", "liberty", "lizard", 
+            "location", "magazine", "marsupial", "melody", "mineral", "miracle", "monument", "mosquito", "musical", "mystery"
+        ]
+    }
+};
