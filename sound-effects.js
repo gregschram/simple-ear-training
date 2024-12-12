4d9fca9d09c2af26739d6f1ccaab0ce57a53580c
gregschram <greg@gregschram.com>
2024-12-11 23:38:35 -0800

    Updated for sfx implementation
    updated index.html, sound-effects.js, and vercel.json

--- sound-effects.js
+++ sound-effects.js
@@ -1,5 +1,156 @@
 // This file is auto-generated. Do not edit manually.
 export const soundEffects = {
     name: "Sound Effects",
-    effects: []
+    effects: [
+  {
+    "id": "airplane",
+    "audioPath": "/audio/sfx/sfx-airplane.mp3",
+    "imagePath": "/images/sfx/img-airplane.svg"
+  },
+  {
+    "id": "applause",
+    "audioPath": "/audio/sfx/sfx-applause.mp3",
+    "imagePath": "/images/sfx/img-applause.svg"
+  },
+  {
+    "id": "baby",
+    "audioPath": "/audio/sfx/sfx-baby.mp3",
+    "imagePath": "/images/sfx/img-baby.svg"
+  },
+  {
+    "id": "bee",
+    "audioPath": "/audio/sfx/sfx-bee.mp3",
+    "imagePath": "/images/sfx/img-bee.svg"
+  },
+  {
+    "id": "bike",
+    "audioPath": "/audio/sfx/sfx-bike.mp3",
+    "imagePath": "/images/sfx/img-bike.svg"
+  },
+  {
+    "id": "bongos",
+    "audioPath": "/audio/sfx/sfx-bongos.mp3",
+    "imagePath": "/images/sfx/img-bongos.svg"
+  },
+  {
+    "id": "cashregister",
+    "audioPath": "/audio/sfx/sfx-cashregister.mp3",
+    "imagePath": "/images/sfx/img-cashregister.svg"
+  },
+  {
+    "id": "cat",
+    "audioPath": "/audio/sfx/sfx-cat.mp3",
+    "imagePath": "/images/sfx/img-cat.svg"
+  },
+  {
+    "id": "chainsaw",
+    "audioPath": "/audio/sfx/sfx-chainsaw.mp3",
+    "imagePath": "/images/sfx/img-chainsaw.svg"
+  },
+  {
+    "id": "cough",
+    "audioPath": "/audio/sfx/sfx-cough.mp3",
+    "imagePath": "/images/sfx/img-cough.svg"
+  },
+  {
+    "id": "dog",
+    "audioPath": "/audio/sfx/sfx-dog.mp3",
+    "imagePath": "/images/sfx/img-dog.svg"
+  },
+  {
+    "id": "doorbell",
+    "audioPath": "/audio/sfx/sfx-doorbell.mp3",
+    "imagePath": "/images/sfx/img-doorbell.svg"
+  },
+  {
+    "id": "drill",
+    "audioPath": "/audio/sfx/sfx-drill.mp3",
+    "imagePath": "/images/sfx/img-drill.svg"
+  },
+  {
+    "id": "engine",
+    "audioPath": "/audio/sfx/sfx-engine.mp3",
+    "imagePath": "/images/sfx/img-engine.svg"
+  },
+  {
+    "id": "goat",
+    "audioPath": "/audio/sfx/sfx-goat.mp3",
+    "imagePath": "/images/sfx/img-goat.svg"
+  },
+  {
+    "id": "honk",
+    "audioPath": "/audio/sfx/sfx-honk.mp3",
+    "imagePath": "/images/sfx/img-honk.svg"
+  },
+  {
+    "id": "horse",
+    "audioPath": "/audio/sfx/sfx-horse.mp3",
+    "imagePath": "/images/sfx/img-horse.svg"
+  },
+  {
+    "id": "liquid",
+    "audioPath": "/audio/sfx/sfx-liquid.mp3",
+    "imagePath": "/images/sfx/img-liquid.svg"
+  },
+  {
+    "id": "owl",
+    "audioPath": "/audio/sfx/sfx-owl.mp3",
+    "imagePath": "/images/sfx/img-owl.svg"
+  },
+  {
+    "id": "pool",
+    "audioPath": "/audio/sfx/sfx-pool.mp3",
+    "imagePath": "/images/sfx/img-pool.svg"
+  },
+  {
+    "id": "rooster",
+    "audioPath": "/audio/sfx/sfx-rooster.mp3",
+    "imagePath": "/images/sfx/img-rooster.svg"
+  },
+  {
+    "id": "saw",
+    "audioPath": "/audio/sfx/sfx-saw.mp3",
+    "imagePath": "/images/sfx/img-saw.svg"
+  },
+  {
+    "id": "scissors",
+    "audioPath": "/audio/sfx/sfx-scissors.mp3",
+    "imagePath": "/images/sfx/img-scissors.svg"
+  },
+  {
+    "id": "seagull",
+    "audioPath": "/audio/sfx/sfx-seagull.mp3",
+    "imagePath": "/images/sfx/img-seagull.svg"
+  },
+  {
+    "id": "shuffle",
+    "audioPath": "/audio/sfx/sfx-shuffle.mp3",
+    "imagePath": "/images/sfx/img-shuffle.svg"
+  },
+  {
+    "id": "sparrow",
+    "audioPath": "/audio/sfx/sfx-sparrow.mp3",
+    "imagePath": "/images/sfx/img-sparrow.svg"
+  },
+  {
+    "id": "telephone",
+    "audioPath": "/audio/sfx/sfx-telephone.mp3",
+    "imagePath": "/images/sfx/img-telephone.svg"
+  },
+  {
+    "id": "thunder",
+    "audioPath": "/audio/sfx/sfx-thunder.mp3",
+    "imagePath": "/images/sfx/img-thunder.svg"
+  },
+  {
+    "id": "typing",
+    "audioPath": "/audio/sfx/sfx-typing.mp3",
+    "imagePath": "/images/sfx/img-typing.svg"
+  },
+  {
+    "id": "zipper",
+    "audioPath": "/audio/sfx/sfx-zipper.mp3",
+    "imagePath": "/images/sfx/img-zipper.svg"
+  }
+]
 };
