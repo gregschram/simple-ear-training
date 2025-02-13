0ce756318feb54ca7498743d79539423102b72f1
gregschram <greg@gregschram.com>
2025-02-13 01:27:53 -0800

    Images- Katt's pics

--- sound-effects.js
+++ sound-effects.js
@@ -5,152 +5,152 @@ export const soundEffects = {
   {
     "id": "airplane",
     "audioPath": "/audio/sfx/sfx-airplane.mp3",
-    "imagePath": "/images/sfx/img-airplane.svg"
+    "imagePath": "/images/sfx/img-airplane.png"
   },
   {
     "id": "applause",
     "audioPath": "/audio/sfx/sfx-applause.mp3",
-    "imagePath": "/images/sfx/img-applause.svg"
+    "imagePath": "/images/sfx/img-applause.png"
   },
   {
     "id": "baby",
     "audioPath": "/audio/sfx/sfx-baby.mp3",
-    "imagePath": "/images/sfx/img-baby.svg"
+    "imagePath": "/images/sfx/img-baby.png"
   },
   {
     "id": "bee",
     "audioPath": "/audio/sfx/sfx-bee.mp3",
-    "imagePath": "/images/sfx/img-bee.svg"
+    "imagePath": "/images/sfx/img-bee.png"
   },
   {
     "id": "bike",
     "audioPath": "/audio/sfx/sfx-bike.mp3",
-    "imagePath": "/images/sfx/img-bike.svg"
+    "imagePath": "/images/sfx/img-bike.png"
   },
   {
     "id": "bongos",
     "audioPath": "/audio/sfx/sfx-bongos.mp3",
-    "imagePath": "/images/sfx/img-bongos.svg"
+    "imagePath": "/images/sfx/img-bongos.png"
   },
   {
     "id": "cashregister",
     "audioPath": "/audio/sfx/sfx-cashregister.mp3",
-    "imagePath": "/images/sfx/img-cashregister.svg"
+    "imagePath": "/images/sfx/img-cashregister.png"
   },
   {
     "id": "cat",
     "audioPath": "/audio/sfx/sfx-cat.mp3",
-    "imagePath": "/images/sfx/img-cat.svg"
+    "imagePath": "/images/sfx/img-cat.png"
   },
   {
     "id": "chainsaw",
     "audioPath": "/audio/sfx/sfx-chainsaw.mp3",
-    "imagePath": "/images/sfx/img-chainsaw.svg"
+    "imagePath": "/images/sfx/img-chainsaw.png"
   },
   {
     "id": "cough",
     "audioPath": "/audio/sfx/sfx-cough.mp3",
-    "imagePath": "/images/sfx/img-cough.svg"
+    "imagePath": "/images/sfx/img-cough.png"
   },
   {
     "id": "dog",
     "audioPath": "/audio/sfx/sfx-dog.mp3",
-    "imagePath": "/images/sfx/img-dog.svg"
+    "imagePath": "/images/sfx/img-dog.png"
   },
   {
     "id": "doorbell",
     "audioPath": "/audio/sfx/sfx-doorbell.mp3",
-    "imagePath": "/images/sfx/img-doorbell.svg"
+    "imagePath": "/images/sfx/img-doorbell.png"
   },
   {
     "id": "drill",
     "audioPath": "/audio/sfx/sfx-drill.mp3",
-    "imagePath": "/images/sfx/img-drill.svg"
+    "imagePath": "/images/sfx/img-drill.png"
   },
   {
     "id": "engine",
     "audioPath": "/audio/sfx/sfx-engine.mp3",
-    "imagePath": "/images/sfx/img-engine.svg"
+    "imagePath": "/images/sfx/img-engine.png"
   },
   {
     "id": "goat",
     "audioPath": "/audio/sfx/sfx-goat.mp3",
-    "imagePath": "/images/sfx/img-goat.svg"
+    "imagePath": "/images/sfx/img-goat.png"
   },
   {
     "id": "honk",
     "audioPath": "/audio/sfx/sfx-honk.mp3",
-    "imagePath": "/images/sfx/img-honk.svg"
+    "imagePath": "/images/sfx/img-honk.png"
   },
   {
     "id": "horse",
     "audioPath": "/audio/sfx/sfx-horse.mp3",
-    "imagePath": "/images/sfx/img-horse.svg"
+    "imagePath": "/images/sfx/img-horse.png"
   },
   {
     "id": "liquid",
     "audioPath": "/audio/sfx/sfx-liquid.mp3",
-    "imagePath": "/images/sfx/img-liquid.svg"
+    "imagePath": "/images/sfx/img-liquid.png"
   },
   {
     "id": "owl",
     "audioPath": "/audio/sfx/sfx-owl.mp3",
-    "imagePath": "/images/sfx/img-owl.svg"
+    "imagePath": "/images/sfx/img-owl.png"
   },
   {
     "id": "pool",
     "audioPath": "/audio/sfx/sfx-pool.mp3",
-    "imagePath": "/images/sfx/img-pool.svg"
+    "imagePath": "/images/sfx/img-pool.png"
   },
   {
     "id": "rooster",
     "audioPath": "/audio/sfx/sfx-rooster.mp3",
-    "imagePath": "/images/sfx/img-rooster.svg"
+    "imagePath": "/images/sfx/img-rooster.png"
   },
   {
     "id": "saw",
     "audioPath": "/audio/sfx/sfx-saw.mp3",
-    "imagePath": "/images/sfx/img-saw.svg"
+    "imagePath": "/images/sfx/img-saw.png"
   },
   {
     "id": "scissors",
     "audioPath": "/audio/sfx/sfx-scissors.mp3",
-    "imagePath": "/images/sfx/img-scissors.svg"
+    "imagePath": "/images/sfx/img-scissors.png"
   },
   {
     "id": "seagull",
     "audioPath": "/audio/sfx/sfx-seagull.mp3",
-    "imagePath": "/images/sfx/img-seagull.svg"
+    "imagePath": "/images/sfx/img-seagull.png"
   },
   {
     "id": "shuffle",
     "audioPath": "/audio/sfx/sfx-shuffle.mp3",
-    "imagePath": "/images/sfx/img-shuffle.svg"
+    "imagePath": "/images/sfx/img-shuffle.png"
   },
   {
     "id": "sparrow",
     "audioPath": "/audio/sfx/sfx-sparrow.mp3",
-    "imagePath": "/images/sfx/img-sparrow.svg"
+    "imagePath": "/images/sfx/img-sparrow.png"
   },
   {
     "id": "telephone",
     "audioPath": "/audio/sfx/sfx-telephone.mp3",
-    "imagePath": "/images/sfx/img-telephone.svg"
+    "imagePath": "/images/sfx/img-telephone.png"
   },
   {
     "id": "thunder",
     "audioPath": "/audio/sfx/sfx-thunder.mp3",
-    "imagePath": "/images/sfx/img-thunder.svg"
+    "imagePath": "/images/sfx/img-thunder.png"
   },
   {
     "id": "typing",
     "audioPath": "/audio/sfx/sfx-typing.mp3",
-    "imagePath": "/images/sfx/img-typing.svg"
+    "imagePath": "/images/sfx/img-typing.png"
   },
   {
     "id": "zipper",
     "audioPath": "/audio/sfx/sfx-zipper.mp3",
-    "imagePath": "/images/sfx/img-zipper.svg"
+    "imagePath": "/images/sfx/img-zipper.png"
   }
 ]
 };
